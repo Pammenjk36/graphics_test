3e26e347c84e1c13694c132a656dff0344b30ae0
Jad K Pamment <jad@jad.localdomain>
2013-03-20 01:29:33 +1300

    Second Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: jad
+ *
+ * Created on March 20, 2013, 12:20 AM
+ */
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Test - Testing commit");
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=graphic_test_app
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/graphic_test_app
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=graphictestapp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/graphictestapp.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=graphic_test_app
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/graphic_test_app
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=graphictestapp.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/graphictestapp.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
